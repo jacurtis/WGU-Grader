3c9a12210d6cf4c6ade9ae7cc684b1bc4f33eafe
J. Alexander Curtis <jalexandercurtis@gmail.com>
2020-12-13 19:12:01 -0700

    Added Images and initial script

--- src/js/in-content.js
+++ src/js/in-content.js
@@ -5,32 +5,24 @@
 * Note that in this scenario the port is open from the popup, but other extensions may open it from the background page or not even have either background.js or popup.js.
 * */
 
-// Extension port to communicate with the popup, also helps detecting when it closes
-let port = null;
+var totalTime = 0.0
+var checkLoaded = setInterval(function() {
+    totalTime += 0.25
+    var assessmentBars = document.getElementsByClassName("assessment__progress-bar__progress")
+    if (assessmentBars.length) {
+       clearInterval(checkLoaded);
+       getWidthAndDisplay(assessmentBars)
+    }
+    console.log("Checked: Total - "+totalTime+"seconds")
+ }, 250); // check every 250ms (aka quarter-second)
 
-// Send messages to the open port (Popup)
-const sendPortMessage = data => port.postMessage(data);
-
-// Handle incoming popup messages
-const popupMessageHandler = message => console.log('in-content.js - message from popup:', message);
-
-// Start scripts after setting up the connection to popup
-chrome.extension.onConnect.addListener(popupPort => {
-    // Listen for popup messages
-    popupPort.onMessage.addListener(popupMessageHandler);
-    // Set listener for disconnection (aka. popup closed)
-    popupPort.onDisconnect.addListener(() => {
-        console.log('in-content.js - disconnected from popup');
-    });
-    // Make popup port accessible to other methods
-    port = popupPort;
-    // Perform any logic or set listeners
-    sendPortMessage('message from in-content.js');
-});
-
-// Response handler for short lived messages
-const handleBackgroundResponse = response =>
-    console.log('in-content.js - Received response:', response);
-
-// Send a message to background.js
-chrome.runtime.sendMessage('Message from in-content.js!', handleBackgroundResponse);
+ // Function to get width
+ function getWidthAndDisplay(assessmentBars) {
+    var elem = assessmentBars[0]
+    var grade = elem.style.width
+    let parsedGrade = parseFloat(grade)
+    if (parsedGrade >= 67 && parsedGrade <= 84) {
+        elem.style.zIndex = 201
+    }
+    elem.innerHTML += `<div style="text-align: right; margin-right: 10px; font-size:2rem; color:white; font-weight: bold;">${grade}</div>`
+ }
