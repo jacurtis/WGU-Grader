5317b0577ee54f70cba671f7d2fd3c6fe7a353d1
J. Alexander Curtis <jalexandercurtis@gmail.com>
2020-12-13 22:03:29 -0700

    Cleaned up in-content script and cleaned up design for popup

--- src/js/in-content.js
+++ src/js/in-content.js
@@ -4,22 +4,29 @@
 * to display the grade to the student.
 * */
 
-// var totalTime = 0.0
+/*
+ * WGU Uses Angular to render their coaching assessments. Unfortunately angular is slow...
+ * This means that Chrome/Firefox/Safari all will trigger this script early when it thinks
+ * the page has loaded, but in reality, Angular hasn't initialized or render its DOM yet.
+ * 
+ * It takes approximately 3-5 seconds to load XHR requests from WGU that provides the 
+ * assessment data. So we need to poll every 1/4 second until we have the data. On the
+ * surface this is very inelegant, but in testing it seems to be working well. It stops polling
+ * after the XHR is recieved, so no long term performance is impacted.
+ */
+
 var checkLoaded = setInterval(function() {
-    // totalTime += 0.25
     var assessmentBars = document.getElementsByClassName("assessment__progress-bar__progress")
     if (assessmentBars.length) {
        clearInterval(checkLoaded);
        getWidthAndDisplay(assessmentBars)
     }
-    // console.log("Checked: Total - "+totalTime+"seconds")
- }, 250); // check every 250ms (aka quarter-second)
+ }, 250);
 
- // Function to get width
- function getWidthAndDisplay(assessmentBars) {
+function getWidthAndDisplay(assessmentBars) {
     var elem = assessmentBars[0]
     var grade = elem.style.width
-    
+
     let parsedGrade = parseFloat(grade)
     if (parsedGrade >= 67 && parsedGrade <= 84) {
         elem.style.zIndex = 201
