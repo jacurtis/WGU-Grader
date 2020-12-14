6b49204f49e7392bb056299bf30470999ffcb6c1
J. Alexander Curtis <jalexandercurtis@gmail.com>
2020-12-13 19:51:55 -0700

    Cleaned up directories to prepare for bundling

--- src/js/in-content.js
+++ src/js/in-content.js
@@ -1,28 +1,34 @@
 /* in-content.js
 *
-* This file has an example on how to communicate with other parts of the extension through a long lived connection (port) and also through short lived connections (chrome.runtime.sendMessage).
-*
-* Note that in this scenario the port is open from the popup, but other extensions may open it from the background page or not even have either background.js or popup.js.
+* This searches the coaching report to find the score a student received, and then injects HTML
+* to display the grade to the student.
 * */
 
-var totalTime = 0.0
+// var totalTime = 0.0
 var checkLoaded = setInterval(function() {
-    totalTime += 0.25
+    // totalTime += 0.25
     var assessmentBars = document.getElementsByClassName("assessment__progress-bar__progress")
     if (assessmentBars.length) {
        clearInterval(checkLoaded);
        getWidthAndDisplay(assessmentBars)
     }
-    console.log("Checked: Total - "+totalTime+"seconds")
+    // console.log("Checked: Total - "+totalTime+"seconds")
  }, 250); // check every 250ms (aka quarter-second)
 
  // Function to get width
  function getWidthAndDisplay(assessmentBars) {
     var elem = assessmentBars[0]
     var grade = elem.style.width
+    
     let parsedGrade = parseFloat(grade)
     if (parsedGrade >= 67 && parsedGrade <= 84) {
         elem.style.zIndex = 201
     }
-    elem.innerHTML += `<div style="text-align: right; margin-right: 10px; font-size:2rem; color:white; font-weight: bold;">${grade}</div>`
+    elem.innerHTML += `<div style="
+                                text-align: right; 
+                                margin-right: 10px; 
+                                font-size:2rem; 
+                                color:white; 
+                                font-weight: bold;"
+                        >${grade}</div>`
  }
